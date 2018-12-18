ecfa73ed66579bf8517b963320173b5fbd2bea9e
Rajasekar T <kt.rajasekar@gmail.com>
2018-12-18 21:22:02 +0530

    Bootstrap CSS added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { CommonModule } from '@angular/common';
+
+//components for routing
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AngularapiComponent } from './angularapi/angularapi.component';
 
 
 const routes: Routes = [
   { path: "dashboard", component: DashboardComponent },
+  { path: "api", component: AngularapiComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 @NgModule({
